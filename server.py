e73203152b1e6aec82fba37e39f449011a4b1eaa
Natalie Oulman <natalie.oulman@gmail.com>
2021-03-02 20:44:02 +0000

    Initial Commit

---
+++ server.py
@@ -0,0 +1,20 @@
+"""Server for Natalie's website."""
+
+from flask import Flask, render_template, request, flash, session, redirect
+from jinja2 import StrictUndefined
+import requests
+import json
+app = Flask(__name__)
+
+#============================     PAGE ROUTES    ==================================#
+
+
+@app.route("/")
+def my_index():
+    """Rendering landing page for Natalie's site"""
+
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
